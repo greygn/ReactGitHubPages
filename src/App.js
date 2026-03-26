c10bdc3c4a57b2cc3f9605dfaf63b14c5414ba73
greygn <serenka.krilov@gmail.com>
2026-03-26 10:34:17 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Link, HashRouter} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Root() {
+    return (
+        <HashRouter>
+            <App></App>
+        </HashRouter>
+    );
 }
 
-export default App;
+function Info() {
+    return <h2>Info</h2>
+}
+
+function Home() {
+    return <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Hello world! =)
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    </div>
+}
+
+function App() {
+    return <div>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+            <Route path="/" component={Home} />
+            <Route path="/info" component={Info} />
+        </Routes>
+    </div>
+}
+
+export default Root;
